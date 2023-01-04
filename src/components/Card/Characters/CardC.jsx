681dcb92a222adcafc5f2c5fdd0905eb62a4b33f
fabi basoni <basoni.fabiany@gmail.com>
2023-01-04 00:06:39 -0300

    feat/characters-css, added saber styling

--- src/components/Card/Characters/CardC.jsx
+++ src/components/Card/Characters/CardC.jsx
@@ -1,7 +1,6 @@
 import styles from './CardC.module.css';
-import { FcClapperboard } from 'react-icons/fc'
 
-const CharacterCard = ({children, onClick}) => {
+const CharacterCard = ({children}) => {
   return (
     <div className={styles.characterCard}>
       <section className={styles.cardHeader}>
@@ -10,17 +9,14 @@ const CharacterCard = ({children, onClick}) => {
       </section>
 
       <ul className={styles.characterInfo}>
-        <li className={styles.description}><strong>Gender:</strong> {children.gender};</li>
+        <li className={styles.characterDescription}><strong>Gender:</strong> {children.gender};</li>
         <li className={styles.characterDescription}><strong>Height:</strong> {children.height}in;</li>
-        <li className={styles.description}> <strong>Mass:</strong>{children.mass}lbs;</li>
-        <li className={styles.description}><strong>Hair color:</strong> {children.hair_color};</li>
-        <li className={styles.description}><strong>Skin color:</strong> {children.skin_color};</li>
-        <li className={styles.description}><strong>Eye color:</strong> {children.eye_color};</li>
-        <li className={styles.description}><strong>Birth year:</strong> {children.birth_year};</li>
+        <li className={styles.characterDescription}> <strong>Mass:</strong>{children.mass}lbs;</li>
+        <li className={styles.characterDescription}><strong>Hair color:</strong> {children.hair_color};</li>
+        <li className={styles.characterDescription}><strong>Skin color:</strong> {children.skin_color};</li>
+        <li className={styles.characterDescription}><strong>Eye color:</strong> {children.eye_color};</li>
+        <li className={styles.characterDescription}><strong>Birth year:</strong> {children.birth_year}.</li>
       </ul>
-      <section className={styles.cardFooter}>
-        <FcClapperboard className={styles.clapperBoard} onClick={onClick}></FcClapperboard>
-      </section>
     </div>
   );
 }
